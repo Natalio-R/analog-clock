1f454bb92023bbbf7a313b659b30652bd21228e4
Natalio Rabasco Navarro <62501046+Natalio-R@users.noreply.github.com>
2021-02-04 20:34:33 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typed", {
+  strings: [`Software`],
+  typeSpeed: 0,
+  startDelay: 0,
+  backSpeed: 0,
+  shuffle: false,
+  backDelay: 0,
+  loop: false,
+  loopCount: false,
+  showCursor: true,
+  cursorChar: "|",
+  contentType: "html",
+});
